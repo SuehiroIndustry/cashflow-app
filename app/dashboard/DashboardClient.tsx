d1a1854f073d18e3c2c424e56396fe2397613095
Takuhiro Tanase <tanase0924@gmail.com>
2026-01-17 17:35:05 +0900

    change

--- app/dashboard/DashboardClient.tsx
+++ app/dashboard/DashboardClient.tsx
@@ -12,27 +12,9 @@ import type { MonthlyBalanceRow } from "./_actions/getMonthlyBalance";
 import { getAccounts } from "./_actions/getAccounts";
 import { getMonthlyBalance } from "./_actions/getMonthlyBalance";
 
+import type { OverviewPayload } from "./_types";
+
 type RiskLevel = "GREEN" | "YELLOW" | "RED" | string;
-
-export type OverviewPayload = {
-  current_balance: number;
-  month_income: number;
-  month_expense: number;
-  net_month: number;
-
-  planned_income_30d: number;
-  planned_expense_30d: number;
-  net_planned_30d: number;
-
-  projected_balance: number;
-  projected_balance_30d: number;
-
-  risk_level: RiskLevel;
-  risk_score: number;
-  computed_at: string | null;
-
-  debug_rows?: unknown;
-};
 
 function yen(n: number) {
   try {
